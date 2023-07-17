bb1eb447a97697b54b9cc193e5fcabe331d2e7ed
aarsalannazari <aarsalannazari@gmail.com>
2023-07-18 01:01:13 +0330

    prettier

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,11 +1,8 @@
 function menu() {
-    var nav_id = document.getElementById("mynav");
-    if(nav_id.className === "nav") {
-        nav_id.className += " responsive";
-    }
-
-    else {
-        nav_id.className = "nav";
-    }
+  var nav_id = document.getElementById("mynav");
+  if (nav_id.className === "nav") {
+    nav_id.className += " responsive";
+  } else {
+    nav_id.className = "nav";
+  }
 }
-
