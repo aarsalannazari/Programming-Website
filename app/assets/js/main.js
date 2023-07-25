e4efbdc85f0d46bfb412bdabfad62b67302a4c6c
aarsalnanazari <aarsalannazari@gmail.com>
2023-07-25 18:04:29 +0330

    first commit

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,8 +1,13 @@
 function menu() {
-  var nav_id = document.getElementById("mynav");
+  var nav_id = document.getElementById("up-nav");
+  let icon_nav = document.getElementById("nav-icon");
   if (nav_id.className === "nav") {
     nav_id.className += " responsive";
+    icon_nav.style.transform = "rotateY(180deg)";
+    icon_nav.style.boxShadow = "1px 1px 1px 1px solid #efefef"
   } else {
     nav_id.className = "nav";
+    icon_nav.style.transform = "rotateY(0deg)"
+    icon_nav.style.boxShadow = "1px 1px 1px 1px solid #efefef"
   }
 }
